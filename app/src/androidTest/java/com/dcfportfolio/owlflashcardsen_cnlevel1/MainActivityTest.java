23450b96444ab9eaeadd9e5d2d13e57f83240ed4
davidfitz314 <davidfitz314@gmail.com>
2019-03-19 02:50:02 -0600

    Adding in new instrumented test cases and deleting the old default example one.

--- app/src/androidTest/java/com/dcfportfolio/owlflashcardsen_cnlevel1/MainActivityTest.java
+++ app/src/androidTest/java/com/dcfportfolio/owlflashcardsen_cnlevel1/MainActivityTest.java
@@ -7,15 +7,14 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Instrumented Tests for MainActivity,
+ * covers onclick button intents and TextView display data.
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MainActivityTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +22,6 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.dcfportfolio.owlflashcardsen_cnlevel1", appContext.getPackageName());
     }
+
+
 }
